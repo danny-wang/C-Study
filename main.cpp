5a241305a199120e0bf7c2980e26165466e3340d
danny <danny.wang@aliyun.com>
2015-07-07 10:20:26 +0800

    Add Student.h

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: danny
+ *
+ * Created on July 7, 2015, 10:18 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
